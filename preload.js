082921f15fb1eceb2db95cfd911de46b0c1d7106
ZiuChen <457353192@qq.com>
2023-04-25 08:47:22 +0000

    Deploying to gh-pages from @ ZiuChen/JSRunner@781dbdfe29462cbf91c5116810058fb3a0fdfa0a 🚀

--- preload.js
+++ preload.js
@@ -1,1 +1,1 @@
-"use strict";window.preload=Object.create(null);const electron=require("electron"),{Buffer}=require("buffer");require("ses"),window.preload={electron,Buffer,require};
+const electron=require("electron"),{Buffer}=require("buffer");require("ses"),window.preload={electron,Buffer,require,Compartment};
