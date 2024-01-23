220eb5ebc3c6980845eef4c62a57bb656b18c117
ZiuChen <457353192@qq.com>
2024-01-23 16:14:13 +0000

    Deploying to gh-pages from @ ZiuChen/JSRunner@ff9c5cd00a28c01e765e4d7bc7d07a31758b2a0a 🚀

--- preload.js
+++ preload.js
@@ -1,1 +1,1 @@
-const{readFileSync}=require("fs"),electron=require("electron"),{Buffer}=require("buffer"),vm=require("vm");window.preload={electron,Buffer,require,vm,readFileSync};
+const electron=require("electron"),{Buffer}=require("buffer"),vm=require("vm");window.preload={electron,Buffer,require,vm};
